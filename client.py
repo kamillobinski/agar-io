4561235849f3d1a6a1ea62705cfcac24c7e05025
Kamil <kamilobinski@gmail.com>
2020-02-01 01:40:45 +0100

    Late init with some work already done

---
+++ client.py
@@ -0,0 +1,105 @@
+""" 
+ * Copyright 2019 Kamil Łobiński <kamilobinski@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. """
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import sys, socket, pickle, threading
+import random, os, pygame, contextlib
+
+""" 
+ *
+ * @Author Kamil Łobiński <kamilobinski@gmail.com>
+ *
+"""
+
+class Network:
+
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = '192.168.56.1'
+        self.port = 55550
+        self.addr = (self.host, self.port)
+
+    def establishConnection(self):
+        self.client.connect(self.addr)
+
+    def sendPlayerUsername(self, username):
+        self.client.send(pickle.dumps(username))
+
+    def sendDataToServer(self, data):
+        try:
+            self.client.send(pickle.dumps(data))
+        except socket.error as e:
+            print(e)
+
+    def disconnectFromServer(self):
+        self.client.close()
+
+        
+
+class Client(QMainWindow):
+
+    def __init__(self, parent=None):
+        super(Client, self).__init__(parent)
+        # Client UI
+        self.initUI()
+        self.refresh()
+
+        # Client networking
+        self.game()
+
+    def initUI(self):
+        vbox = QVBoxLayout()
+
+        window = QWidget()
+        window.setLayout(vbox)
+        self.setCentralWidget(window)
+
+        self.setGeometry(2720, 400, 300, 300)
+        self.setWindowTitle('Game client')
+        self.show()
+
+    def game(self):
+        global players
+
+        self.server = Network()
+        self.server.establishConnection()
+
+        username = input("Please enter your name: ")
+        threading.Thread(target=self.server.sendPlayerUsername(username)).start()
+
+        self.send()
+
+    def send(self):
+        data = input("Please enter data to send: ")
+        threading.Thread(target=self.server.sendPlayerUsername(data)).start()
+        self.send()
+        
+    def closeEvent(self, event):
+        self.server.disconnectFromServer()
+        print('zamykam')
+
+
+
+    def refresh(self):
+        self.update()
+
+if __name__ == '__main__':
+   app = QApplication([])
+   client = Client()
+   sys.exit(app.exec_())
+
+   
---
+++ server.py
@@ -0,0 +1,147 @@
+""" 
+ * Copyright 2019 Kamil Łobiński <kamilobinski@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. """
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import sys, socket, pickle, threading
+import subprocess
+
+""" 
+ *
+ * @Author Kamil Łobiński <kamilobinski@gmail.com>
+ *
+"""
+
+actual_connections = {}
+
+class Network:
+
+    def __init__(self):
+        self.serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = '192.168.56.1'
+        self.port = 55550
+        self.addr = (self.host, self.port)
+
+        global actual_connections
+        self.client_id = 0
+
+    def runServer(self):
+        self.prepareServerForConnection()
+        threading.Thread(target=self.serverLoop).start()
+
+    def prepareServerForConnection(self):
+        self.serv.bind(self.addr)
+        self.serv.listen(5) 
+
+    def serverLoop(self):
+        while True:
+            try:
+                server.addInformation('[INFO] Server waiting for new connection')
+                conn, address = self.serv.accept()
+                server.addInformation('[CONNECTION] Server established new connection')
+                threading.Thread(target=self.serverLoop).start()
+                threading.Thread(target=self.getClientUsername(conn, address)).start()
+                threading.Thread(target=self.handleClientMessages(conn, address, self.client_id)).start()
+            
+            except Exception as ex:
+                #server.textArea.insertPlainText('[ERROR] Server caught error.')
+                pass
+
+    def getClientUsername(self, connection, address):
+        self.client_id = self.client_id + 1
+
+        data = connection.recv(16) 
+        name = pickle.loads(data)
+        name = str(name)
+        server.addInformation('[CLIENT] Client ' + name)
+        actual_connections[self.client_id] = {'name':name}
+
+
+    def handleClientMessages(self, connection, address, client_id):
+
+        while True:
+            data = connection.recv(2048)
+            if not data:
+                server.addInformation('[CONNECTION] Client ' + name + ' disconnected from server')
+                break
+            if data == b'':
+                pass
+            else:       
+                data_reveived = pickle.loads(data)
+                server.addInformation('[CLIENT] Client ' + actual_connections[client_id]['name'] + ' sent: ' + str(data_reveived) )
+
+        connection.close()
+
+
+
+class Server(QMainWindow):
+
+    def __init__(self, parent=None):
+        super(Server, self).__init__(parent)
+        self.initUI()
+
+        self.network = Network()
+        self.network.runServer()
+
+        self.fillServerProperties()
+        self.refresh()
+
+    def initUI(self):
+        self.createComponents()
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.serverPropertiesGroup)
+        vbox.addWidget(self.textArea)
+
+        window = QWidget()
+        window.setLayout(vbox)
+        self.setCentralWidget(window)
+
+        self.setGeometry(2400, 400, 300, 300)
+        self.setWindowTitle('Game server')
+
+    def createComponents(self):
+        self.serverIP = QLabel('IP: ', self)
+        self.serverPORT = QLabel('Port: ', self)
+
+        serverPropertiesGroupLayout = QVBoxLayout()
+        serverPropertiesGroupLayout.addWidget(self.serverIP)
+        serverPropertiesGroupLayout.addWidget(self.serverPORT)
+        
+        self.serverPropertiesGroup = QGroupBox('Server properties')
+        self.serverPropertiesGroup.setLayout(serverPropertiesGroupLayout)
+
+        self.textArea = QPlainTextEdit()
+        self.textArea.setReadOnly(True)
+                
+
+    def fillServerProperties(self):
+        self.serverIP.setText('IP: ' + self.network.host)
+        self.serverPORT.setText('Port: ' + str(self.network.port))
+
+    def addInformation(self, info):
+        self.textArea.insertPlainText(info + '\n')
+
+    def refresh(self):
+        self.update()
+
+if __name__ == '__main__':
+    app = QApplication([])
+    server = Server()
+    server.show()
+    sys.exit(app.exec_())
+
+   
